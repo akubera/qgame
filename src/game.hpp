a246d52184bfe949301f81049939b41e591f8504
Andrew Kubera <andrewkubera@gmail.com>
2015-09-08 19:26:17 -0400

    Added documentation to classes

--- src/game.hpp
+++ src/game.hpp
@@ -5,8 +5,14 @@
 #ifndef GAME_H_
 #define GAME_H_
 
+/**
+ * \class Game
+ * \brief The game... I guess?
+ *
+ */
 class Game {
 public:
+  /// Default Constructor
   Game();
 
 protected:
--- src/player.hpp
+++ src/player.hpp
@@ -7,14 +7,30 @@
 
 #include <string>
 
+/**
+ * \class Player
+ * \brief Player class which plays the game
+ */
 class Player {
 public:
+  /**
+   * Default constructor, sets name to 'noname'
+   */
   Player();
+
+  /**
+   * Named constructor
+   * Sets name to provided name
+   *
+   * \param name Name of the player
+   */
   Player(std::string name);
 
+  /// Return name of the player
   std::string getName() const;
 
 protected:
+  /// name of the player
   std::string _name;
 };
 
